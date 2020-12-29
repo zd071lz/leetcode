2335ff9a70e930c6e52cc266143145c944d3399e
zd071lz <zd071lz@126.com>
2020-12-29 22:50:07 +0800

    solution

---
+++ src/leetcode/dp/Leetcode1641.java
@@ -0,0 +1,45 @@
+package leetcode.dp;
+
+/**
+ * 1641. 统计字典序元音字符串的数目
+ * 给你一个整数 n，请返回长度为 n 、仅由元音 (a, e, i, o, u) 组成且按 字典序排列 的字符串数量。
+ * 字符串 s 按 字典序排列 需要满足：对于所有有效的 i，s[i] 在字母表中的位置总是与 s[i+1] 相同或在 s[i+1] 之前。
+ *  
+ * 示例 1：
+ * 输入：n = 1
+ * 输出：5
+ * 解释：仅由元音组成的 5 个字典序字符串为 ["a","e","i","o","u"]
+ * 示例 2：
+ * 输入：n = 2
+ * 输出：15
+ * 解释：仅由元音组成的 15 个字典序字符串为
+ * ["aa","ae","ai","ao","au","ee","ei","eo","eu","ii","io","iu","oo","ou","uu"]
+ * 注意，"ea" 不是符合题意的字符串，因为 'e' 在字母表中的位置比 'a' 靠后
+ * 示例 3：
+ * 输入：n = 33
+ * 输出：66045
+ *  
+ * 提示：
+ * 1 <= n <= 50 
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-sorted-vowel-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Leetcode1641 {
+    public int countVowelStrings(int n) {
+        int[][] dp = new int[5][n];
+        for (int i = 0; i < 5; i++) {
+            dp[i][0] = i + 1;
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[4][n - 1];
+    }
+}
