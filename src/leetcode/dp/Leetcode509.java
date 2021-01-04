4ce59b9ef8e2731d65e805feee7b06b03da2ddc2
lizheng <zheng.li1@okcoin.net>
2021-01-04 11:54:19 +0800

    Solution

---
+++ src/leetcode/dp/Leetcode509.java
@@ -0,0 +1,19 @@
+package leetcode.dp;
+
+/**
+ * 509. 斐波那契数
+ */
+public class Leetcode509 {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
