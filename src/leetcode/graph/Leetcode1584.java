7216bc253f82c753ba0f5297d2ac43b5fe905bc8
lizheng <zheng.li1@okcoin.net>
2021-01-20 09:51:49 +0800

    solution

--- src/leetcode/graph/Leetcode1584.java
+++ src/leetcode/graph/Leetcode1584.java
@@ -1,44 +1,161 @@
 package leetcode.graph;
 
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * 1584. 连接所有点的最小费用
  * 给你一个points 数组，表示 2D 平面上的一些点，其中 points[i] = [xi, yi] 。
+ *
  * 连接点 [xi, yi] 和点 [xj, yj] 的费用为它们之间的 曼哈顿距离 ：|xi - xj| + |yi - yj| ，其中 |val| 表示 val 的绝对值。
+ *
  * 请你返回将所有点连接的最小总费用。只有任意两点之间 有且仅有 一条简单路径时，才认为所有点都已连接。
+ *
+ * 示例 1：
+ *
+ * 输入：points = [[0,0],[2,2],[3,10],[5,2],[7,0]]
+ * 输出：20
+ * 解释：
+ *
+ * 我们可以按照上图所示连接所有点得到最小总费用，总费用为 20 。
+ * 注意到任意两个点之间只有唯一一条路径互相到达。
+ * 示例 2：
+ *
+ * 输入：points = [[3,12],[-2,5],[-4,1]]
+ * 输出：18
+ * 示例 3：
+ *
+ * 输入：points = [[0,0],[1,1],[1,0],[-1,1]]
+ * 输出：4
+ * 示例 4：
+ *
+ * 输入：points = [[-1000000,-1000000],[1000000,1000000]]
+ * 输出：4000000
+ * 示例 5：
+ *
+ * 输入：points = [[0,0]]
+ * 输出：0
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= points.length <= 1000
+ * -106 <= xi, yi <= 106
+ * 所有点 (xi, yi) 两两不同。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/min-cost-to-connect-all-points
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Leetcode1584 {
-    PriorityQueue<int[]> pq;
-    boolean[] marked;
     public int minCostConnectPoints(int[][] points) {
-        int ans = 0;
-        marked = new boolean[points.length];
-        pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        visit(points, 0);
-        while (!pq.isEmpty()) {
-            int[] poll = pq.poll();
-            int u = poll[0];
-            int w = poll[1];
-            if (marked[u]) {
-                continue;
-            } else {
-                ans += w;
-                visit(points, u);
+        int n = points.length;
+        DisjointSetUnion dsu = new DisjointSetUnion(n);
+        List<Edge> edges = new ArrayList<Edge>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                edges.add(new Edge(dist(points, i, j), i, j));
             }
         }
-        return ans;
+        Collections.sort(edges, new Comparator<Edge>() {
+            public int compare(Edge edge1, Edge edge2) {
+                return edge1.len - edge2.len;
+            }
+        });
+        int ret = 0, num = 1;
+        for (Edge edge : edges) {
+            int len = edge.len, x = edge.x, y = edge.y;
+            if (dsu.unionSet(x, y)) {
+                ret += len;
+                num++;
+                if (num == n) {
+                    break;
+                }
+            }
+        }
+        return ret;
     }
 
-    public void visit(int[][] points, int v) {
-        marked[v] = true;
-        for (int u = 0; u < points.length; u++) {
-            if (!marked[u]) {
-                int w = Math.abs(points[v][0] - points[u][0])
-                        + Math.abs(points[v][1] - points[u][1]);
-                pq.offer(new int[]{u, w});
-            }
+    public int dist(int[][] points, int x, int y) {
+        return Math.abs(points[x][0] - points[y][0]) + Math.abs(points[x][1] - points[y][1]);
+    }
 
+
+    private class DisjointSetUnion {
+        int[] f;
+        int[] rank;
+        int n;
+
+        public DisjointSetUnion(int n) {
+            this.n = n;
+            this.rank = new int[n];
+            Arrays.fill(this.rank, 1);
+            this.f = new int[n];
+            for (int i = 0; i < n; i++) {
+                this.f[i] = i;
+            }
+        }
+
+        public int find(int x) {
+            return f[x] == x ? x : (f[x] = find(f[x]));
+        }
+
+        public boolean unionSet(int x, int y) {
+            int fx = find(x), fy = find(y);
+            if (fx == fy) {
+                return false;
+            }
+            if (rank[fx] < rank[fy]) {
+                int temp = fx;
+                fx = fy;
+                fy = temp;
+            }
+            rank[fx] += rank[fy];
+            f[fy] = fx;
+            return true;
+        }
+    }
+
+    private class Edge {
+        int len, x, y;
+
+        public Edge(int len, int x, int y) {
+            this.len = len;
+            this.x = x;
+            this.y = y;
         }
     }
 }
+
+
+//    PriorityQueue<int[]> pq;
+//    boolean[] marked;
+//    public int minCostConnectPoints(int[][] points) {
+//        int ans = 0;
+//        marked = new boolean[points.length];
+//        pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+//        visit(points, 0);
+//        while (!pq.isEmpty()) {
+//            int[] poll = pq.poll();
+//            int u = poll[0];
+//            int w = poll[1];
+//            if (marked[u]) {
+//                continue;
+//            } else {
+//                ans += w;
+//                visit(points, u);
+//            }
+//        }
+//        return ans;
+//    }
+//
+//    public void visit(int[][] points, int v) {
+//        marked[v] = true;
+//        for (int u = 0; u < points.length; u++) {
+//            if (!marked[u]) {
+//                int w = Math.abs(points[v][0] - points[u][0])
+//                        + Math.abs(points[v][1] - points[u][1]);
+//                pq.offer(new int[]{u, w});
+//            }
+//
+//        }
+//    }
